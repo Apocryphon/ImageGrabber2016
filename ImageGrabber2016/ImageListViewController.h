22de3bb9bdac90706e8f5dd120b49c369a034c19
Richard Yeh <apocryphon.now@gmail.com>
2016-09-05 22:25:37 -0700

    credited Ray Wenderlich

--- ImageGrabber2016/ImageListViewController.h
+++ ImageGrabber2016/ImageListViewController.h
@@ -5,6 +5,9 @@
 //  Created by Richard Yeh on 9/5/16.
 //  Copyright © 2016 Situation Excellent. All rights reserved.
 //
+//  Original Version Created by Ray Wenderlich on 7/3/11.
+//  Copyright 2011 Ray Wenderlich. All rights reserved.
+//
 
 #import <UIKit/UIKit.h>
 #import "ImageManager.h"
